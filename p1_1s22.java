6bf5686969fb0288ec9fd703fe33004db4b7893e
Eduardo5974 <144293055+Eduardo5974@users.noreply.github.com>
2023-09-06 19:17:33 -0600

    Add files via upload

---
+++ p1_1s22.java
@@ -0,0 +1,7 @@
+Class p1_1s22{
+	public static void main (String[]args) {
+	System.out.println("Estamos empezando a programar");
+	System.out.println("esta es una segunda linea");
+	
+}
+}
---
+++ p2_1s22.java
@@ -0,0 +1,9 @@
+Class p2_1s22{
+	public static void main (String[]args) {
+	System.out.println("Cota Ramos Luis Eduardo");
+	System.out.println("18 años");
+	System.out.println("cotaramosluiseduardo@gmail.com");
+	System.out.println("me gustan los videojuegos");
+	Systen.out.println("morado");
+}
+}
