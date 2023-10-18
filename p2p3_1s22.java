958c0f1ced8e25047001f991abbc1264aae61e88
Eduardo5974 <144293055+Eduardo5974@users.noreply.github.com>
2023-10-17 19:28:18 -0600

    Add files via upload

---
+++ p2p3_1s22.java
@@ -0,0 +1,20 @@
+public class p2p3_1s22{
+	public static void main(String[] args) {
+		int a=0;
+
+		while(a<=9){
+			System.out.println("el valor dea vale "+a);
+			a++;
+		}
+		while(true){
+			System.out.println("nuevamente a vale" +a);
+			if (a>=9) break;
+			a++;
+		}
+		boolean estado = false;
+
+		while(estado){
+			System.out.println("se mostrara esta linea?");
+		}
+	}
+}	
